aff59fb55c47ceb25d2b8839449a4c66026ea5e7
Jack McGill <mr.mcgilljh@gmail.com>
2021-05-30 10:18:49 +1000

    Modify post header and add style.

--- single.php
+++ single.php
@@ -18,17 +18,19 @@ get_header();
 
 			get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'jazzclub' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'jazzclub' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
 			endif;
+
+            the_post_navigation(
+                array(
+                    'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'jazzclub' ) . '</span> <span class="nav-title">%title</span>',
+                    'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'jazzclub' ) . '</span> <span class="nav-title">%title</span>',
+                )
+            );
 
 		endwhile; // End of the loop.
 		?>
--- template-parts/content.php
+++ template-parts/content.php
@@ -11,6 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+        <?php jazzclub_the_category_list(); ?>
 		<?php
 		if ( is_singular() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
@@ -22,8 +23,8 @@
 			?>
 			<div class="entry-meta">
 				<?php
-				jazzclub_posted_on();
 				jazzclub_posted_by();
+				jazzclub_posted_on();
 				?>
 			</div><!-- .entry-meta -->
 		<?php endif; ?>
